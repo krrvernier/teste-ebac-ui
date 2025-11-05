1b71a7d3a695b0bae394ae37dd98fd188942cb03
Kim Vernier <krrvernier@gmail.com>
2025-11-05 11:27:21 -0300

    fakers e variáveis

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -1,4 +1,4 @@
-///reference types="cypress/>"
+/// <reference types="cypress"/>
 describe('Funcionalidade:cadastro', () => {
     beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
