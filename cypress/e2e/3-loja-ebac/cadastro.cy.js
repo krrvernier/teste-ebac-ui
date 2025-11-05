320474566fabbe07850334c026dd019501470106
Kim Vernier <krrvernier@gmail.com>
2025-11-05 10:57:54 -0300

    faker e variáveis

---
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -0,0 +1,51 @@
+///reference types="cypress/>"
+describe('Funcionalidade:cadastro', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('Deve completar cadastro com sucesso', () => {
+        cy.get('#reg_email').type('user123@gmail.com')
+        cy.get('#reg_password').type('senha@123')
+        cy.get(':nth-child(4) > .button').click()
+        //deve executar somente na primeira vez e a partir da segunda tentativa deve mostrar mensagem de erro
+    });
+});
+
+//Usando faker-js
+///reference types-"cypress/>"
+import { faker} from '@faker-js/faker'
+describe('Funcionalidade:cadastro com faker', () => {
+    beforeEach(() => {
+         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('Deve completar cadastro com sucesso', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('senha@1234')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+    });
+});
+
+//Com variáveis
+///reference types-"cypress/>"
+describe('Funcionalidade: cadastro usando variáveis', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    it.only('Deve completar cadastro com sucesos - usando variáveis', () => {
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+        var email = faker.internet.email(nome)
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('senha@1234')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+    });
+});
